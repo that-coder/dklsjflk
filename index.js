b6e490eb147b69fa1ed272788257ca40d0148c35
Jayakrishnan <thatcoder.space@gmail.com>
2022-07-06 11:43:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const dklsjflk = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello dklsjflk`}))
+  res.end()
+  
+}
+
+export default dklsjflk
